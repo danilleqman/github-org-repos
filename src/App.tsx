b6fed0cc131112472b07287c92dd5cfc8fb26bf6
Danil Leiman <danilleqman@gmail.com>
2020-08-14 15:54:02 +0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Container } from "@material-ui/core";
+import { OrgReposPage } from "./pages/OrgReposPage";
 
 function App() {
-  return <Container>hello</Container>;
+  return (
+    <Container>
+      <OrgReposPage />
+    </Container>
+  );
 }
 
 export default App;
