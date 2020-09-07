c0f881ebccd80d010deedc8c61cac09afd395b6b
Danil Leiman <danilleqman@gmail.com>
2020-09-07 20:14:22 +0300

    Updates

---
+++ precache-manifest.ccab6bea511eec5aa2859428c1c99d96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbd18bd6a1f469e9042f164835fc299",
+    "url": "/github-org-repos/index.html"
+  },
+  {
+    "revision": "6b986bd2122b98a62e06",
+    "url": "/github-org-repos/static/js/2.6532e654.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github-org-repos/static/js/2.6532e654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f034e03e02125160212",
+    "url": "/github-org-repos/static/js/main.7a971f11.chunk.js"
+  },
+  {
+    "revision": "5445cb3a2d7aac82421d",
+    "url": "/github-org-repos/static/js/runtime-main.31475ed5.js"
+  }
+]);
